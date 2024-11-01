7fe998d2867af1aab73310be05e5b17a557f8972
ramame2 <s1206152@student.windesheim.nl>
2024-11-01 03:52:38 +0100

    Update over.php

--- over.php
+++ over.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Over - Rama Mari</title>
+    <link rel="icon" href="my.ico" type="image/x-icon">
     <style>
         :root {
             --bg-color-light: #f0f0f0; --text-color-light: #333;
