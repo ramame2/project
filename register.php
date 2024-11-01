21558568b92afce85fb79bb638d1d99ce567e23b
ramame2 <s1206152@student.windesheim.nl>
2024-11-01 03:52:41 +0100

    Update register.php

--- register.php
+++ register.php
@@ -41,6 +41,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registreren</title>
+    <link rel="icon" href="my.ico" type="image/x-icon">
     <link rel="stylesheet" href="me3.css">
     <style>
         body {
