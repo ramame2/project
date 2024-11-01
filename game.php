97a147606ba314d919d0226a9b519c78edb1d300
ramame2 <s1206152@student.windesheim.nl>
2024-11-01 03:54:50 +0100

    Update game.php

--- game.php
+++ game.php
@@ -40,6 +40,7 @@ echo $row['high_score']
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bubble Game</title>
+    <link rel="icon" href="my.ico" type="image/x-icon">
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
