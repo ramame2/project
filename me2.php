2153ccd61e789257a1e288b1b6e9ad7d983a4981
ramame2 <s1206152@student.windesheim.nl>
2024-11-01 12:11:43 +0100

    Update me2.php

--- me2.php
+++ me2.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 if (!isset($_SESSION['user_id'])) {
-    header("Location: login.php"); // Redirect to login if not logged in
+    header("Location: login.php");
     exit;
 }
 ?>
