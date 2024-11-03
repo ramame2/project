5dc5fe54a9b4eeccb30ce746c5c56ce909da0a6d
ramame2 <s1206152@student.windesheim.nl>
2024-11-03 02:44:14 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Letter by Letter Text Animation</title>
+
     <style>
         body {
             display: flex;
